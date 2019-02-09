48c0aa9e45dea742bb0fdaa67b3eb11d29ca1259
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2019-02-09 19:06:32 +0100

    Added script the executes a 10 fold cross validation

---
+++ Code/R-script/10foldcross.R
@@ -0,0 +1,54 @@
+# source a set of functions
+source("categorical.R")
+
+# set working directory
+#setwd("G:/My Drive/appunti/Gio ed Elia/Magistrale/Primo Semestre/Data Technology and Machine Learning/Progetto/Dataset/WIP/Giorgia/code/R-script")
+setwd("~/MosquitoDiseaseSpreadRatePrediction/Code/R-script")
+
+if(!require("randomForest")){
+  install.packages("randomForest")
+}
+library(randomForest)
+
+# load input file
+dataset <- read.table("features_dataset_final.csv", header = TRUE, sep =",", dec = ".")
+dataset_with_only_features_and_target <- get_features_dataset(dataset)
+
+#Randomly shuffle the data
+set.seed(100)
+
+#Create 10 equally size folds
+folds <- cut(seq(1,nrow(dataset_with_only_features_and_target)),breaks=10,labels=FALSE)
+
+#Perform 10 fold cross validation
+for(i in 1:10){
+  #Segement your data by fold using the which() function 
+  Crosstrain <- which(folds==i,arr.ind=TRUE)
+  CrossTrainSet <- dataset_with_only_features_and_target[Crosstrain, ]
+  CrossValidSet <- dataset_with_only_features_and_target[-Crosstrain, ]
+  
+  #Use the test and train data partitions however you desire...
+  
+  # this model use as features all the columns except for that with more than 53 categories
+  model0 <- randomForest(result ~ ., data = CrossTrainSet, importance = TRUE)
+  model0
+  
+  # show model error
+  plot(model0, ylim=c(0,0.36))
+  legend('topright', colnames(model0$err.rate), col=1:3, fill=1:3)
+  ## check importance variable
+  importance(model0)        
+  varImpPlot(model0) 
+  
+  predTrain0 <- predict(model0, CrossTrainSet, type = "class")
+  
+  # checking classification accuracy
+  table(predTrain0, CrossTrainSet$result) 
+  
+  ## predicting on Validation set
+  predValid0 <- predict(model0, CrossValidSet, type = "class")
+  
+  # checking classification accuracy
+  acc_m0<- mean(predValid0 == CrossValidSet$result)                    
+  table(predValid0,CrossValidSet$result)
+}
