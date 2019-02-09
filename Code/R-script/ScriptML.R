19297a9767a06161df73c636e7f50e4ed8050513
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2019-02-09 19:30:27 +0100

    Moved up some part of code (for readability)

--- Code/R-script/ScriptML.R
+++ Code/R-script/ScriptML.R
@@ -1,18 +1,9 @@
-# source a set of functions
-source("categorical.R")
-
 # set working directory
 #setwd("G:/My Drive/appunti/Gio ed Elia/Magistrale/Primo Semestre/Data Technology and Machine Learning/Progetto/Dataset/WIP/Giorgia/code/R-script")
 setwd("~/MosquitoDiseaseSpreadRatePrediction/Code/R-script")
 
-# load input file
-dataset <- read.table("data-1549540504937.csv", header = TRUE, sep =",", dec = ".")
-dataset_with_only_features_and_target <- get_features_dataset(dataset)
-
-# some statistics ...
-head(dataset)
-str(dataset)
-summary(dataset)
+# source a set of functions
+source("categorical.R")
 
 if(!require("Amelia")){
   install.packages("Amelia")
@@ -29,6 +20,28 @@ if(!require("lattice")){
 }
 library(lattice)
 
+if(!require("devtools")){
+  install.packages("devtools")
+}
+library(devtools)
+devtools::install_github('araastat/reprtree')
+
+if(!require("reprtree")){
+  install.packages("reprtree")
+}
+library(reprtree)
+
+if(!require("randomForest")){
+  install.packages("randomForest")
+}
+library(randomForest)
+
+if(!require("ROCR")){
+  install.packages("ROCR")
+}
+library(ROCR)
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 missmap(dataset, main = "Missing values vs observed")
 
 # split into Train and Validation sets: 80 - 20 (random)
@@ -39,29 +52,12 @@ ValidSet <- dataset_with_only_features_and_target[-train,]
 summary(TrainSet)
 summary(ValidSet)
 
-if(!require("randomForest")){
-  install.packages("randomForest")
-}
-library(randomForest)
-
 # this model use as features all the columns except for that with more than 53 categories
 model0 <- randomForest(result ~ ., data = TrainSet, importance = TRUE)
 model0
 
 #show tree
 getTree(model0, 1, labelVar=TRUE)
-
-if(!require("devtools")){
-  install.packages("devtools")
-}
-library(devtools)
-devtools::install_github('araastat/reprtree')
-
-
-if(!require("reprtree")){
-  install.packages("reprtree")
-}
-library(reprtree)
 
 reprtree:::plot.getTree(model0)
 
