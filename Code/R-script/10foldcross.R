da0984cffd651f6ee1e2291f51097e4600096e5d
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2019-02-10 11:30:57 +0100

    Fixed error in 10flodcross validation and added plot of AUC

--- Code/R-script/10foldcross.R
+++ Code/R-script/10foldcross.R
@@ -1,5 +1,6 @@
 # source a set of functions
 source("categorical.R")
+source("utilities.R")
 
 # set working directory
 #setwd("G:/My Drive/appunti/Gio ed Elia/Magistrale/Primo Semestre/Data Technology and Machine Learning/Progetto/Dataset/WIP/Giorgia/code/R-script")
@@ -20,6 +21,16 @@ set.seed(100)
 #Create 10 equally size folds
 folds <- cut(seq(1,nrow(dataset_with_only_features_and_target)),breaks=10,labels=FALSE)
 
+model0 <- list()
+acc_mT0 <- list()
+acc_mV0 <- list()
+precision_positive <- list()
+recall_positive <- list()
+f.score_positive <- list()
+precision_negative <- list()
+recall_negative <- list()
+f.score_negative <- list()
+
 #Perform 10 fold cross validation
 for(i in 1:10){
   #Segement your data by fold using the which() function 
@@ -30,25 +41,32 @@ for(i in 1:10){
   #Use the test and train data partitions however you desire...
   
   # this model use as features all the columns except for that with more than 53 categories
-  model0[i] <- randomForest(result ~ ., data = CrossTrainSet, importance = TRUE)
-  model0[i]
+  model0[[i]] <- randomForest(result ~ ., data = CrossTrainSet, importance = TRUE)
   
   # show model error
-  plot(model0[i], ylim=c(0,1.2))
-  legend('topright', colnames(model0[i]$err.rate), col=1:3, fill=1:3)
-  ## check importance variable
-  importance(model0[i])        
-  varImpPlot(model0[i]) 
+  plot(model0[[i]], ylim=c(0,1.2))
+  legend('topright', colnames(model0[[i]]$err.rate), col=1:3, fill=1:3)
   
-  predTrain0[i] <- predict(model0[i], CrossTrainSet, type = "class")
+  ## check importance variable
+  importance(model0[[i]])        
+  varImpPlot(model0[[i]]) 
+  
+  predTrain0 <- predict(model0[[i]], CrossTrainSet, type = "prob")
+  predTrain0_True <- predTrain0[,2]
   
   # checking classification accuracy
-  table(predTrain0[i], CrossTrainSet$result) 
+  acc_mT0[i] <- mean(predTrain0_True == CrossTrainSet$result) 
+  table(predTrain0_True, CrossTrainSet$result) 
   
   ## predicting on Validation set
-  predValid0[i] <- predict(model0[i], CrossValidSet, type = "class")
+  predValid0 <- predict(model0[[i]], CrossValidSet, type = "prob")
+  predValid0_True <- predValid0[,2]
   
   # checking classification accuracy
-  acc_m0 <- mean(predValid0[i] == CrossValidSet$result) 
-  table(predValid0[i],CrossValidSet$result)
+  acc_mV0[i] <- mean(predValid0_True == CrossValidSet$result) 
+  table(predValid0_True, CrossValidSet$result)
+  
+  # plot auc
+  plot_auc(predTrain0_True, CrossTrainSet$result, i) # train
+  plot_auc(predValid0_True, CrossValidSet$result, i) # valid
 }
