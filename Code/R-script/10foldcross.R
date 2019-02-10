2a6a9cdb3acece6417b9c7fbe395c1c2385cb2e2
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2019-02-10 16:20:18 +0100

    Updated r-script
    added plot for holdout model

--- Code/R-script/10foldcross.R
+++ Code/R-script/10foldcross.R
@@ -28,19 +28,24 @@ crossValidation <- function(dataset) {
     model0[[i]] <<- randomForest(result ~ ., data = CrossTrainSet, importance = TRUE)
     
     # show model error
+    pdf(paste("CrossValidationRF/model_error_", i, ".pdf", sep=""))
     plot(model0[[i]], ylim=c(0,1.2))
     legend('topright', colnames(model0[[i]]$err.rate), col=1:3, fill=1:3)
+    dev.off()
     
     ## check importance variable
     print(importance(model0[[i]]))   
+    pdf(paste("CrossValidationRF/model_importance_", i, ".pdf", sep=""))
     varImpPlot(model0[[i]]) 
+    dev.off()
     
     predTrain0 <<- predict(model0[[i]], CrossTrainSet, type = "prob")
     predTrain0_True <<- predTrain0[,2]
     
     # checking classification accuracy
     acc_mT0[i] <<- mean(predTrain0_True == CrossTrainSet$result) 
-    print(table(predTrain0_True, CrossTrainSet$result))
+    capture.output(acc_mT0, file = paste("CrossValidationRF/acc_mT0_2", i, ".txt", sep=""))
+    capture.output(table(predTrain0_True, CrossTrainSet$result), file = paste("CrossValidationRF/train_accuracy_", i, ".txt", sep=""))
     
     ## predicting on Validation set
     predValid0 <<- predict(model0[[i]], CrossValidSet, type = "prob")
@@ -48,10 +53,11 @@ crossValidation <- function(dataset) {
     
     # checking classification accuracy
     acc_mV0[i] <<- mean(predValid0_True == CrossValidSet$result) 
-    print(table(predValid0_True, CrossValidSet$result))
+    capture.output(acc_mT0, file = paste("CrossValidationRF/acc_mV0_", i, ".txt", sep=""))
+    capture.output(table(predValid0_True, CrossValidSet$result), file = paste("CrossValidationRF/valid_accuracy_", i, ".txt", sep=""))
     
     # plot auc
-    plot_auc(predTrain0_True, CrossTrainSet$result, i) # train
-    plot_auc(predValid0_True, CrossValidSet$result, i) # valid
+    plot_auc(predTrain0_True, CrossTrainSet$result, i, "CrossValidationRF/train") # train
+    plot_auc(predValid0_True, CrossValidSet$result, i, "CrossValidationRF/validation") # valid
   }
 }
--- Code/R-script/holdout.R
+++ Code/R-script/holdout.R
@@ -1,44 +1,50 @@
 holdout <- function(dataset) {
   # split into Train and Validation sets: 80 - 20 (random)
   set.seed(100)
-  train <<- sample(nrow(dataset), 0.8*nrow(dataset), replace = FALSE)
-  TrainSet <<- dataset[train,]
-  ValidSet <<- dataset[-train,]
+  train <<- sample(nrow(dataset_with_only_features_and_target), 0.8*nrow(dataset_with_only_features_and_target), replace = FALSE)
+  TrainSet <<- dataset_with_only_features_and_target[train,]
+  ValidSet <<- dataset_with_only_features_and_target[-train,]
   
   # statistics
   summary(TrainSet)
   summary(ValidSet)
   
   # this model use as features all the columns except for that with more than 53 categories
-  model0 <<- randomForest(result ~ ., data = TrainSet, importance = TRUE)
+  model1 <<- randomForest(result ~ ., data = TrainSet, importance = TRUE)
   
   #show tree
-  getTree(model0, 1, labelVar=TRUE)
+  getTree(model1, 1, labelVar=TRUE)
   
-  reprtree:::plot.getTree(model0)
+  reprtree:::plot.getTree(model1)
   
   # show model error
-  plot(model0, ylim=c(0,1.2))
-  legend('topright', colnames(model0$err.rate), col=1:3, fill=1:3)
+  pdf("HoldoutRF/model_error.pdf") 
+  plot(model1, ylim=c(0,1.2))
+  legend('topright', colnames(model1$err.rate), col=1:3, fill=1:3)
+  dev.off() 
   
   # check importance variable
-  importance(model0)        
-  varImpPlot(model0) 
+  importance(model1)   
+  pdf("HoldoutRF/model_importance.pdf")
+  varImpPlot(model1) 
+  dev.off() 
   
-  predTrain0 <<- predict(model0, TrainSet, type = "prob")
-  predTrain0_True <<- predTrain0[,2]
+  predTrain1 <<- predict(model1, TrainSet, type = "prob")
+  predTrain1_True <<- predTrain1[,2]
   
   # checking classification accuracy
-  table(predTrain0_True, TrainSet$result) 
-  acc_mT0 <<- mean(predTrain0_True == TrainSet$result)   
+  capture.output(table(predTrain1_True, TrainSet$result), file = "HoldoutRF/train_accuracy.txt")
+  acc_mT1 <<- mean(predTrain1_True == TrainSet$result)   
+  capture.output(acc_mT1, file = "HoldoutRF/acc_mT1.txt")
   
   ## predicting on Validation set
-  predValid0 <<- predict(model0, ValidSet, type = "prob")
-  predValid0_True <<- predValid0[,2]
+  predValid1 <<- predict(model1, ValidSet, type = "prob")
+  predValid1_True <<- predValid1[,2]
   
   # checking classification accuracy
-  acc_m0 <<- mean(predValid0_True == ValidSet$result)                    
-  table(predValid0_True, ValidSet$result)
+  acc_mV1 <<- mean(predValid1_True == ValidSet$result)                    
+  capture.output(acc_mV1, file = "HoldoutRF/acc_mV1.txt")
+  capture.output(table(predValid1_True, ValidSet$result), file = "HoldoutRF/valid_accuracy.txt")
   
   precision_positive <<- list()
   recall_positive <<- list()
@@ -48,6 +54,6 @@ holdout <- function(dataset) {
   f.score_negative <<- list()
   
   # plot auc
-  plot_auc(predTrain0_True, TrainSet$result, 1) # train
-  plot_auc(predValid0_True, ValidSet$result, 1) # valid
+  plot_auc(predTrain1_True, TrainSet$result, 1, "HoldoutRF/train") # train
+  plot_auc(predValid1_True, ValidSet$result, 1, "HoldoutRF/validation") # valid
 }
